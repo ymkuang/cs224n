2495724b4400ecdd447cf7f3e87fe0d877aef146
ymkuang <yumingkuang@gmail.com>
2014-10-03 02:29:22 -0700

    IBMModel2 done.

--- pa1/java/src/cs224n/wordaligner/IBMModel2.java
+++ pa1/java/src/cs224n/wordaligner/IBMModel2.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.lang.Math;
 
+
 /**
  * Simple word alignment baseline model that maps source positions to target 
  * positions along the diagonal of the alignment grid.
@@ -16,35 +17,72 @@ import java.lang.Math;
  * 
  * @author Yuming Kuang
  */
-public class IBMModel1 implements WordAligner {
+public class IBMModel2 implements WordAligner {
+  
+  private class TriNumber {
+    protected int i, n, m;
+
+    public TriNumber(int newI, int newN, int newM) {
+      i = newI;
+      n = newN;
+      m = newM;
+    }
+
+    public int getFirst() {
+      return i;
+    }
+
+    public int getSecond() {
+      return n;
+    }
+
+    public int getThird() {
+      return m;
+    }
+
+    public boolean equals(Object o) {
+      if (o == null) return false;
+      if (!(o instanceof TriNumber)) return false;
+
+      TriNumber other = (TriNumber) o;
+      if (this.i != other.i) return false;
+      if (this.n != other.n) return false;
+      if (this.m != other.m) return false;
+
+      return true;
+    }
+
+    public int hashCode() {
+      return this.i * 100 + this.n * 100 + this.m;
+    }
+  }
+
 
   private static final long serialVersionUID = 1315751943476440515L;
   
   // TODO: Use arrays or Counters for collecting sufficient statistics
   // from the training data.
-  private CounterMap<String,String> lexicalProb;
-
-  public CounterMap<String, String> getLexicalProb() {
-    return lexicalProb;
-  }
+  private CounterMap<String, String> lexicalProb;
+  private CounterMap<TriNumber, Integer> alignProb;
 
   public Alignment align(SentencePair sentencePair) {
     // Placeholder code below. 
     // TODO Implement an inference algorithm for Eq.1 in the assignment
     // handout to predict alignments based on the counts you collected with train().
     Alignment alignment = new Alignment();
-    int numSourceWords = sentencePair.getSourceWords().size();
-    int numTargetWords = sentencePair.getTargetWords().size();
+    int m = sentencePair.getSourceWords().size();
+    int n = sentencePair.getTargetWords().size();
 
     // Alignment for each target word
-    for (int tgtIndex = 0; tgtIndex < numTargetWords; tgtIndex++) {
+    for (int tgtIndex = 0; tgtIndex < n; tgtIndex++) {
       // Compute source word with largest t(e|f)
       int alignIndex = -1;
       double maxProb = -1.0;
-      for (int srcIndex = -1; srcIndex < numSourceWords; srcIndex++) {
+      for (int srcIndex = -1; srcIndex < m; srcIndex++) {
         String sourceWord = srcIndex == -1 ? "" : sentencePair.getSourceWords().get(srcIndex);
         String targetWord = sentencePair.getTargetWords().get(tgtIndex);
-        double prob = lexicalProb.getCount(sourceWord, targetWord);
+        TriNumber position = new TriNumber(srcIndex, n, m);
+        double prob = lexicalProb.getCount(sourceWord, targetWord) * alignProb.getCount(position, tgtIndex);
         if (prob > maxProb) {
            alignIndex = srcIndex;
            maxProb = prob;
@@ -60,51 +98,74 @@ public class IBMModel1 implements WordAligner {
   }
 
   public void train(List<SentencePair> trainingPairs) {
+    IBMModel1 ibmModel1 = new IBMModel1();
+    ibmModel1.train(trainingPairs);
     // Initialize
-    lexicalProb = new CounterMap<String, String>();
+    lexicalProb = ibmModel1.getLexicalProb();
     CounterMap<String, String> oldLexicalProb;
+    alignProb = new CounterMap<TriNumber, Integer>();
+    CounterMap<TriNumber, Integer> oldAlignProb;
+
     double delta = 1;
     int iter = 0;
     // Iteration
     while (delta > 1e-4 && iter < 50) {
       oldLexicalProb = lexicalProb;
+      oldAlignProb = alignProb;
       lexicalProb = new CounterMap<String, String>();
+      alignProb = new CounterMap<TriNumber, Integer>();
       iter ++;
 
       // E step
       for(SentencePair pair : trainingPairs){
         List<String> targetWords = pair.getTargetWords();
         List<String> sourceWords = pair.getSourceWords();
-      
-        for (String target : targetWords) {
+        int n = targetWords.size();
+        int m = sourceWords.size();
+        
+        for (int tgtIndex = 0; tgtIndex < n; tgtIndex ++) {
           // Compute sum(t(e_i|f_j))
+          String target = targetWords.get(tgtIndex);
           double totalProb = 0;
-          for (int srcIndex = -1; srcIndex < sourceWords.size(); srcIndex ++) {
+          for (int srcIndex = -1; srcIndex < m; srcIndex ++) {
+            TriNumber position = new TriNumber(srcIndex, n, m);
             String source = (srcIndex == -1 ? "" : sourceWords.get(srcIndex));
-            totalProb += (iter == 1 ? 1 : oldLexicalProb.getCount(source, target));
+            totalProb += oldLexicalProb.getCount(source, target) * (iter == 1 ? 1.0 / (m + 1) : oldAlignProb.getCount(position, tgtIndex));
           }
           // Update M step
-          for (int srcIndex = -1; srcIndex < sourceWords.size(); srcIndex ++) {
+          for (int srcIndex = -1; srcIndex < m; srcIndex ++) {
+            TriNumber position = new TriNumber(srcIndex, n, m);
             String source = (srcIndex == -1 ? "" : sourceWords.get(srcIndex));
-            double increment = (iter == 1 ? 1 : oldLexicalProb.getCount(source, target)) / totalProb; 
+            double increment = oldLexicalProb.getCount(source, target) * (iter == 1 ? 1.0 / (m + 1) : oldAlignProb.getCount(position, tgtIndex)) / totalProb; 
             if (lexicalProb.getCount(source, target) == 0.0) {
               lexicalProb.setCount(source, target, increment);
             } else {
               lexicalProb.incrementCount(source, target, increment);
+            }
+
+            if (alignProb.getCount(position, tgtIndex) == 0.0) {
+              alignProb.setCount(position, tgtIndex, increment);
+            } else {
+              alignProb.incrementCount(position, tgtIndex, increment);
             }
           }   
         }
       }
       // M step normalize
       lexicalProb = Counters.conditionalNormalize(lexicalProb);
+      alignProb = Counters.conditionalNormalize(alignProb);
       
       // Compute difference
-      delta = iter == 1 ? 1 : computeDiff(lexicalProb, oldLexicalProb);
+      if (iter == 1) delta = 1; 
+      else {
+        delta = computeDiff(lexicalProb, oldLexicalProb);
+        delta += computeDiff(alignProb, oldAlignProb); 
+      }
       System.out.printf("%f\n", delta);
     } 
   }
 
-  private <K> double computeDiff(CounterMap<K, K> newCnt, CounterMap<K, K> oldCnt) {
+  private <K, V> double computeDiff(CounterMap<K, V> newCnt, CounterMap<K, V> oldCnt) {
     double delta = 0;
     int cnt = 0;    
 
@@ -114,11 +175,11 @@ public class IBMModel1 implements WordAligner {
   
     for (K key : allKey) {
       // All values for a key in two CounterMap
-      Set<K> allValue = new HashSet<K>(newCnt.getCounter(key).keySet());
+      Set<V> allValue = new HashSet<V>(newCnt.getCounter(key).keySet());
       allValue.addAll(oldCnt.getCounter(key).keySet());
 
       // Compute difference for each (key, value) pair
-      for (K value : allValue) {
+      for (V value : allValue) {
         delta += Math.abs(newCnt.getCount(key, value) - oldCnt.getCount(key, value));
         cnt ++;
       }
