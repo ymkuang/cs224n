66218f026b2cf064bd94baeb9089a057fdfd5dc8
ymkuang <yumingkuang@gmail.com>
2014-11-04 16:33:41 -0800

    Done with rulebased

--- pa3/java/src/cs224n/corefsystems/RuleBased.java
+++ pa3/java/src/cs224n/corefsystems/RuleBased.java
@@ -13,7 +13,52 @@ import cs224n.coref.*;
 import cs224n.util.Pair;
 import cs224n.ling.Tree;
 
+
+
 public class RuleBased implements CoreferenceSystem {
+    public static class TreeNode {
+    	public Tree<String> node;
+    	public int sentenceIndex;
+    	public int beginWordIndex;
+    	public List<TreeNode> children;
+    	
+    	public TreeNode(Tree<String> node, int sentenceIndex, int beginWordIndex) {
+    		this.node = node;
+    		this.sentenceIndex = sentenceIndex;
+    		this.beginWordIndex = beginWordIndex;
+    		this.children = Collections.emptyList();
+    	}
+    	
+        public void setChildren(List<TreeNode> children) {
+        	this.children = children;
+        }
+
+        public List<TreeNode> getChildren() {
+        	return children;
+        }
+
+    	@Override
+    	public boolean equals(Object o){
+    		if(o instanceof TreeNode){
+		        TreeNode other = (TreeNode) o;
+		        return other.node.equals(this.node) && 
+		             other.sentenceIndex == this.sentenceIndex && 
+		             other.beginWordIndex == this.beginWordIndex;
+		    } else {
+		        return false;
+		    }
+		}
+
+		@Override
+		public int hashCode() {
+			final int key = 17;
+			int code = 1;
+			code = key * code + node.hashCode();
+			code = key * code + sentenceIndex;
+			code = key * code + beginWordIndex;
+			return code; 
+		}
+    }
     private Map<String, Set<String>> headCluster;
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
@@ -53,10 +98,11 @@ public class RuleBased implements CoreferenceSystem {
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
+		//System.out.printf("New document.-----------------\n");
 		Map<Mention, Set<Mention>> clusterMap = new HashMap<Mention, Set<Mention>>();
 		List<Mention> pronouns = new ArrayList<Mention>();
-		Map<Tree<String>, Tree<String>> parentMap = constructParentMap(doc);
-		Map<Tree<String>, Mention> parseMap = constructParseMap(doc);
+		Map<TreeNode, TreeNode> parentMap = constructParentMap(doc);
+		Map<TreeNode, Mention> parseMap = constructParseMap(doc);
 
 		// First Pass: Exact match
 		Map<String, Set<Mention>> exactMap = new HashMap<String, Set<Mention>>();
@@ -64,13 +110,15 @@ public class RuleBased implements CoreferenceSystem {
 			String word = m.gloss();
 			if (Pronoun.isSomePronoun(word)) {
 				pronouns.add(m);
-			} else if (exactMap.containsKey(word)) {
-				exactMap.get(word).add(m);
-				clusterMap.put(m, exactMap.get(word));
-			} else {
-				Set<Mention> cluster = new HashSet<Mention>();
-				cluster.add(m);
-				clusterMap.put(m, cluster);
+				continue;
+			} 
+			Set<Mention> cluster;
+			if (exactMap.containsKey(word)) cluster = exactMap.get(word);
+			else cluster = new HashSet<Mention>();
+			cluster.add(m);
+			exactMap.put(word, cluster);
+			for (Mention mention : cluster) {	
+				clusterMap.put(mention, cluster);
 			}
 		}
 
@@ -79,8 +127,8 @@ public class RuleBased implements CoreferenceSystem {
 			for (Mention m2 : clusterMap.keySet()) {
 				if (m1.equals(m2)) continue;
                 if (clusterMap.get(m1).equals(clusterMap.get(m2))) continue;
-				if (isAppositive(m1, m2, parentMap) && isAgree(m1, m2)) {
-					System.out.printf("Appositive: %s  AND  %s\n", m1.gloss(), m2.gloss());
+				if (isAppositive(m1, m2, doc, parentMap) && isAgree(m1, m2)) {
+					//System.out.printf("Appositive: %s  AND  %s\n", m1.gloss(), m2.gloss());
 					mergeSet(m1, m2, clusterMap);
 				}
 			}
@@ -92,19 +140,22 @@ public class RuleBased implements CoreferenceSystem {
 				if (m1.equals(m2)) continue;
                 if (clusterMap.get(m1).equals(clusterMap.get(m2))) continue;
 				if (isHeadMatch(m1, m2) && isAgree(m1, m2)) {
-					System.out.printf("Head Match: %s  AND  %s\n", m1.gloss(), m2.gloss());
+					//System.out.printf("Head Match: %s  AND  %s\n", m1.gloss(), m2.gloss());
 					mergeSet(m1, m2, clusterMap);
 				}
 			}
 		}
 
 		// Fourth Pass: Pronouns
+		Set<Mention> quotedPronoun = new HashSet<Mention>();
 		for (Mention m : pronouns) {
 			Mention mention = null;
 			if (m.headToken().isQuoted()) mention = quotedPronoun(m, doc);
+			//if (mention != null )
+            //	System.out.printf("Quoted Match: %s  AND  %s\n", m.gloss(), mention.gloss());
             if (mention == null) mention = hobbs(doc, m, parentMap, parseMap);
-            if (mention != null )
-            	System.out.printf("Pronoun Match: %s  AND  %s\n", m.gloss(), mention.gloss());
+            //if (mention != null )
+            //	System.out.printf("Pronoun Match: %s  AND  %s\n", m.gloss(), mention.gloss());
             
             Set<Mention> mCluster = clusterMap.get(m);
             Set<Mention> mentionCluster = (mention == null ? null : clusterMap.get(mention));
@@ -144,12 +195,18 @@ public class RuleBased implements CoreferenceSystem {
         return mentions;
 	}
 	
-	private boolean isAppositive(Mention m1, Mention m2, Map<Tree<String>, Tree<String>> parentMap) {
+	private boolean isAppositive(Mention m1, Mention m2, Document doc, 
+		Map<TreeNode, TreeNode> parentMap) {
+		
 		if (m1 == null || m2 == null) return false;
 		Sentence s1 = m1.sentence, s2 = m2.sentence;
 		if (!s1.equals(s2)) return false;
 		if (!m1.parse.getLabel().equals("NP") || !m2.parse.getLabel().equals("NP")) return false;
-		Tree<String> parent1 = parentMap.get(m1.parse), parent2 = parentMap.get(m2.parse);
+		TreeNode node1 = new TreeNode(m1.parse, doc.indexOfSentence(m1.sentence), m1.beginIndexInclusive),
+		         node2 = new TreeNode(m2.parse, doc.indexOfSentence(m2.sentence), m2.beginIndexInclusive);
+		node1 = parentMap.get(node1); node2 = parentMap.get(node2);
+		if (node1 == null || node2 == null) return false;
+		Tree<String> parent1 = node1.node, parent2 = node2.node;
 		if (parent1 == null || parent2 == null) return false;
 		if (parent1.equals(parent2)) return false;
 		Tree<String> parent = parent1;
@@ -168,9 +225,8 @@ public class RuleBased implements CoreferenceSystem {
         boolean genderAgree = (genderAgreeInfo.getFirst() && genderAgreeInfo.getSecond()) || !genderAgreeInfo.getFirst();
         Pair<Boolean, Boolean> numberAgreeInfo = Util.haveNumberAndAreSameNumber(m1, m2);
         boolean numberAgree = (numberAgreeInfo.getFirst() && numberAgreeInfo.getSecond()) || !numberAgreeInfo.getFirst();
-        boolean nerAgree = isNerAgree(m1, m2);
         boolean speakerAgree = isSpeakerAgree(m1, m2);
-        return genderAgree && numberAgree && nerAgree && speakerAgree;
+        return genderAgree && numberAgree && speakerAgree;
     }
 
     private boolean isSpeakerAgree(Mention m1, Mention m2) {
@@ -225,55 +281,71 @@ public class RuleBased implements CoreferenceSystem {
     	return clusterSet;
     }
 
-    private Map<Tree<String>, Tree<String>> constructParentMap(Document doc) {
-        Map<Tree<String>, Tree<String>> parentMap = new HashMap<Tree<String>, Tree<String>>();
+    private Map<TreeNode, TreeNode> constructParentMap(Document doc) {
+        Map<TreeNode, TreeNode> parentMap = new HashMap<TreeNode, TreeNode>();
         for (Sentence s : doc.sentences) {
-        	constructParentMapHelper(s.parse, parentMap);
+        	constructParentMapHelper(s.parse, null, 0, doc.indexOfSentence(s), parentMap);
         }
         return parentMap;
     }
 
-    private void constructParentMapHelper(Tree<String> node, Map<Tree<String>, Tree<String>> parentMap) {
-        if (node == null) return;
-        for (Tree<String> child : node.getChildren()) {
-        	constructParentMapHelper(child, parentMap);
-        	parentMap.put(child, node);
+    private Pair<TreeNode, Integer> constructParentMapHelper(Tree<String> node, TreeNode parent, 
+    	int currentWordIndex, int sentenceIndex, Map<TreeNode, TreeNode> parentMap) {
+        
+        TreeNode cur = new TreeNode(node, sentenceIndex, currentWordIndex);
+        if (node.isLeaf()) {
+        	currentWordIndex ++;
+        } else {
+        	List<TreeNode> children = new ArrayList<TreeNode>();
+            for (Tree<String> child : node.getChildren()) {
+                Pair<TreeNode, Integer> res = constructParentMapHelper(child, cur, currentWordIndex, sentenceIndex, parentMap);
+        	    children.add(res.getFirst());
+        	    currentWordIndex = res.getSecond();
+        	}
+        	cur.setChildren(children);
         }
+        if (parent != null) {
+        	//System.out.printf("%s %d %d -> %s %d %d\n", cur.node.getLabel(), cur.sentenceIndex, cur.beginWordIndex,
+        	//  parent.node.getLabel(), parent.sentenceIndex, parent.beginWordIndex);
+        	parentMap.put(cur, parent);
+        }
+        return new Pair<TreeNode, Integer>(cur, currentWordIndex);
     }
 
-    private Map<Tree<String>, Mention> constructParseMap(Document doc) {
-    	Map<Tree<String>, Mention> parseMap = new HashMap<Tree<String>, Mention>();
+    private Map<TreeNode, Mention> constructParseMap(Document doc) {
+    	Map<TreeNode, Mention> parseMap = new HashMap<TreeNode, Mention>();
     	for (Mention m : doc.getMentions()) {
-    		parseMap.put(m.parse, m);
+    		parseMap.put(new TreeNode(m.parse, doc.indexOfSentence(m.sentence), m.beginIndexInclusive), m);
     	}
     	return parseMap;
     }
 
-    private Tree<String> traceUp(Tree<String> cur, int labelType, 
-    	Map<Tree<String>, Tree<String>> parentMap, Set<Tree<String>> path) {
+    private TreeNode traceUp(TreeNode cur, int labelType, 
+    	Map<TreeNode, TreeNode> parentMap, Set<TreeNode> path) {
     	if (cur == null) return null;
     	if (path != null) path.clear();
     	while (true) {
     		if (path != null) path.add(cur);
 
-            System.out.printf("%s ->", cur.getLabel());
+            //System.out.printf("%s ->", cur.node.getLabel());
     		cur = parentMap.get(cur);
     		if (cur == null) break;
-    		System.out.printf("%s\n", cur.getLabel());
+    		//System.out.printf("%s\n", cur.node.getLabel());
 
-    		if (cur.getLabel().equals("NP") || (labelType == 1 && cur.getLabel().equals("S")))
+    		if (cur.node.getLabel().equals("NP") || 
+    			(labelType == 1 && (cur.node.getLabel().equals("S") )))
     			return cur;
     	}
     	return null;
     }
 
-    private void traverse(Tree<String> start, int traverseType, 
-    	Set<Tree<String>> path, LinkedList<Tree<String>> candidates) {
+    private void traverse(TreeNode start, int traverseType, 
+    	Set<TreeNode> path, LinkedList<TreeNode> candidates) {
     	if (start == null) return;
-    	LinkedList<Tree<String>> queue = new LinkedList<Tree<String>>();
+    	LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
     	
     	boolean beforePath = true;
-    	for (Tree<String> node : start.getChildren()) {
+    	for (TreeNode node : start.getChildren()) {
             if (traverseType != 0 && path.contains(node)) {
             	beforePath = false;
             	continue;
@@ -285,10 +357,10 @@ public class RuleBased implements CoreferenceSystem {
     	} 
 
     	while (!(queue.size() == 0)) {
-    		Tree<String> node = queue.remove();
-            if (node.getLabel().equals("NP")) candidates.add(node);
-            if (traverseType != 2 || !node.getLabel().equals("NP")) {
-            	for (Tree<String> child : node.getChildren()) {
+    		TreeNode node = queue.remove();
+            if (node.node.getLabel().equals("NP")) candidates.add(node);
+            if (traverseType != 2 || !node.node.getLabel().equals("NP")) {
+            	for (TreeNode child : node.getChildren()) {
             		queue.add(child);
             	}
             }
@@ -296,93 +368,99 @@ public class RuleBased implements CoreferenceSystem {
 
     }
 
-    private Mention findCandidate(LinkedList<Tree<String>> candidates, Mention m, 
-    	Map<Tree<String>, Mention> parseMap) {
+    private Mention findCandidate(LinkedList<TreeNode> candidates, Mention m, 
+    	Map<TreeNode, Mention> parseMap) {
         
         while (!(candidates.size() == 0)) {
-        	Tree<String> node = candidates.remove();
+        	TreeNode node = candidates.remove();
             Mention candidate = parseMap.get(node);   
-            if (candidate != null) System.out.printf("%s \n", candidate.gloss()); 
+            //if (candidate != null) System.out.printf("%s \n", candidate.gloss()); 
             if (candidate != null && isAgree(candidate, m)) return candidate;
         }
         return null;
     }
 
-    private boolean isNonHeadPhrase(Tree<String> cur, Set<Tree<String>> path) {
+    private boolean isNonHeadPhrase(TreeNode cur, Set<TreeNode> path) {
         if (path == null) return false;
-        for (Tree<String> node : path) {
-        	if (!node.getLabel().equals("NP") && !node.getLabel().equals("NNS") 
-        		 && !node.getLabel().equals("NN") && !node.getLabel().equals("NNP") 
-        		 && !node.getLabel().equals("NNPS"))
+        for (TreeNode node : path) {
+        	if (node.equals(cur)) continue;
+        	if (!node.node.getLabel().equals("NP") && !node.node.getLabel().equals("NNS") 
+        		 && !node.node.getLabel().equals("NN") && !node.node.getLabel().equals("NNP") 
+        		 && !node.node.getLabel().equals("NNPS"))
         		return true;
         }
         return false;
     }
 
     private Mention hobbs(Document doc, Mention m, 
-    	Map<Tree<String>, Tree<String>> parentMap, Map<Tree<String>, Mention> parseMap) {
-    	System.out.printf("Pronoun: %s\n", m.gloss());
-    	System.out.printf("%s\n", m.sentence.parse.toString());
-    	Tree<String> cur = m.parse;
-    	if (cur != null) System.out.printf("%s\n", cur.getLabel());
+    	Map<TreeNode, TreeNode> parentMap, Map<TreeNode, Mention> parseMap) {
+    	final int MAX_PREVIOUS_SENTENCE = 1;
+    	//System.out.printf("Pronoun: %s\n", m.gloss());
+    	//System.out.printf("%s\n", m.sentence.parse.toString());
     	Sentence s = m.sentence; 
-        Set<Tree<String>> path = new HashSet<Tree<String>>();
-        LinkedList<Tree<String>> candidates = new LinkedList<Tree<String>>();
+    	TreeNode cur = new TreeNode(m.parse, doc.indexOfSentence(s), m.beginIndexInclusive);
+        Set<TreeNode> path = new HashSet<TreeNode>();
+        LinkedList<TreeNode> candidates = new LinkedList<TreeNode>();
         Mention candidate;
+        
+        int prevSentenceIndex = doc.indexOfSentence(s) - MAX_PREVIOUS_SENTENCE;
+        if (prevSentenceIndex < 0) prevSentenceIndex = 0;
 
         // Step 1
-        System.out.printf("Step 1\n");
-        if (!cur.getLabel().equals("NP")) cur = traceUp(cur, 0, parentMap, null);
-        if (cur != null) System.out.printf("%s\n", cur.getLabel());
+        //System.out.printf("Step 1\n");
+        if (!cur.node.getLabel().equals("NP")) cur = traceUp(cur, 0, parentMap, null);
+        //if (cur != null) System.out.printf("%s\n", cur.node.getLabel());
         if (cur == null) return null;
         
         // Step 2
-        System.out.printf("Step 2\n");
+        //System.out.printf("Step 2\n");
         cur = traceUp(cur, 1, parentMap, path);
-        if (cur != null) System.out.printf("%s\n", cur.getLabel());
-        if (cur == null) return null;
+        //if (cur != null) System.out.printf("%s\n", cur.node.getLabel());
+        if (cur == null) 
+        	cur = parentMap.get(new TreeNode(s.parse.getChildren().get(0), doc.indexOfSentence(s), 0));
 
         // Step 3
-        System.out.printf("Step 3\n");
+        //System.out.printf("Step 3\n");
         traverse(cur, 1, path, candidates);
         candidate = findCandidate(candidates, m, parseMap);
         if (candidate != null) return candidate;
 
         while (true) {
             // Step 4
-            if (cur.equals(s.parse)) {
-            	System.out.printf("Step 4\n");
+            if (cur.node.equals(s.parse)) {
+            	//System.out.printf("Step 4\n");
             	int sentenceIndex = doc.indexOfSentence(s);
-            	if (sentenceIndex == 0) break;
-            	s = doc.sentences.get(sentenceIndex - 1);
-                cur = s.parse;
+            	sentenceIndex--;
+            	if (sentenceIndex < prevSentenceIndex) break;
+            	s = doc.sentences.get(sentenceIndex);
+                cur = parentMap.get(new TreeNode(s.parse.getChildren().get(0), sentenceIndex, 0));
                 traverse(cur, 0, null, candidates);
                 candidate = findCandidate(candidates, m, parseMap);
                 if (candidate != null) return candidate;
             } else {
             	// Step 5
-            	System.out.printf("Step 5\n");
+            	//System.out.printf("Step 5\n");
             	cur = traceUp(cur, 1, parentMap, path);
             	if (cur == null) {
-            		cur = s.parse;
+            		cur = new TreeNode(s.parse, doc.indexOfSentence(s), 0);
             		continue;
             	}
             	
             	// Step 6
-            	System.out.printf("Step 6\n");
+            	//System.out.printf("Step 6\n");
             	if (isNonHeadPhrase(cur, path)) candidates.add(cur);
             	candidate = findCandidate(candidates, m, parseMap);
                 if (candidate != null) return candidate;
 
                 // Step 7
-                System.out.printf("Step 7\n");
+                //System.out.printf("Step 7\n");
                 traverse(cur, 1, path, candidates);
                 candidate = findCandidate(candidates, m, parseMap);
                 if (candidate != null) return candidate;
 
                 // Step 8
-                System.out.printf("Step 8\n");
-                if (cur.getLabel().equals("S")) traverse(cur, 2, path, candidates);
+                //System.out.printf("Step 8\n");
+                if (cur.node.getLabel().equals("S")) traverse(cur, 2, path, candidates);
                 candidate = findCandidate(candidates, m, parseMap);
                 if (candidate != null) return candidate;
             }
